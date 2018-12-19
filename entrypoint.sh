af2a93cf60d6c0dca83c09ac32d12199223a8bd7
Kyle Lucy <kmlucy@gmail.com>
2018-12-19 07:29:27 -0500

    update for static dir url fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ python3 manage.py migrate
 # Add base url if set
 if [ "$BASE_URL" != "http://localhost:8000" ]; then
 	BASE_URL_DIR=$(echo "$BASE_URL" | sed -e 's=.*\://==' -e 's=[^/]*==')
-	sed -i -e "s='/static/'='""$BASE_URL_DIR""/static/'=" -e "s=http://localhost:8000=$BASE_URL=" hrss/settings.py
+	sed -i -e "s=http://localhost:8000=$BASE_URL=" hrss/settings.py
 fi
 
 # Allow all connections
